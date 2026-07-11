6de040c8fd3be1dc49ea40cab860dfce7c0ca897
K01dBy4spam935h <junjunlahpai@gmail.com>
2026-07-11 22:47:05 +1200

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,25 @@
+-- Crypt-MM2-Legit | Config
+-- Put all your asset IDs and preferences here.
+
+_G.Config = {
+
+    -- Main background image (shown behind the UI window)
+    -- Upload at roblox.com/develop > Decals, paste the NUMBER only
+    BackgroundDecal = "rbxassetid://10341849885",
+
+    -- Icon shown when UI is minimized (small square button)
+    MinimizeIconDecal = "rbxassetid://5649884823",
+
+    -- Keybind to toggle UI visibility (minimized <-> open)
+    ToggleKey = Enum.KeyCode.Insert,
+
+    -- UI starting position
+    StartPosition = UDim2.new(0.5, -260, 0.5, -200),
+
+    -- Default window size
+    WindowSize = UDim2.new(0, 520, 0, 400),
+
+    -- Minimized icon size
+    IconSize = UDim2.new(0, 54, 0, 54),
+
+}
