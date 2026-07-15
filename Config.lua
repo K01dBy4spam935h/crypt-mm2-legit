8f558b10e1dbbf575be014e90ea0eddd83bca0a8
K01dBy4spam935h <junjunlahpai@gmail.com>
2026-07-15 13:41:59 +1200

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -3,18 +3,18 @@
 -- Upload your PNG there, copy the NUMBER from the URL — use rbxassetid://NUMBER
 
 _G.Config = {
-    BackgroundDecal    = "rbxassetid://0",   -- replace 0 with your Image ID
-    MinimizeIconDecal  = "rbxassetid://0",   -- replace 0 with your Image ID
+    BackgroundDecal    = "rbxassetid://6311243701",   -- replace 0 with your Image ID
+    MinimizeIconDecal  = "rbxassetid://5649884823",   -- replace 0 with your Image ID
 
-    Icon_Main          = "rbxassetid://0",
-    Icon_Aimbot        = "rbxassetid://0",
-    Icon_Visuals       = "rbxassetid://0",
-    Icon_LocalPlayer   = "rbxassetid://0",
-    Icon_Murderer      = "rbxassetid://0",
-    Icon_Sheriff       = "rbxassetid://0",
-    Icon_Farm          = "rbxassetid://0",
-    Icon_Teleport      = "rbxassetid://0",
-    Icon_Settings      = "rbxassetid://0",
+    Icon_Main          = "rbxassetid://13060262582",
+    Icon_Aimbot        = "rbxassetid://108977717302566",
+    Icon_Visuals       = "rbxassetid://13321848342",
+    Icon_LocalPlayer   = "rbxassetid://124871982298256",
+    Icon_Murderer      = "rbxassetid://6187718258",
+    Icon_Sheriff       = "rbxassetid://15571371474",
+    Icon_Farm          = "rbxassetid://111098288810374",
+    Icon_Teleport      = "rbxassetid://6723742959",
+    Icon_Settings      = "rbxassetid://11932591121",
 
     ToggleKey          = Enum.KeyCode.RightControl,
     WindowSize         = UDim2.new(0, 560, 0, 440),
