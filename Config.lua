24f2137238d202f274c530228c59a4ba335d7fe4
K01dBy4spam935h <junjunlahpai@gmail.com>
2026-07-16 20:59:06 +1200

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -3,8 +3,31 @@
 -- Upload your PNG there, copy the NUMBER from the URL — use rbxassetid://NUMBER
 
 _G.Config = {
-    BackgroundDecal    = "rbxassetid://7255938910",   -- replace 0 with your Image ID
-    MinimizeIconDecal  = "rbxassetid://10716387829",   -- replace 0 with your Image ID
+    BackgroundDecal    = "0",   -- replace 0 with your Image ID
+    MinimizeIconDecal  = "0",   -- replace 0 with your Image ID
+
+        -- In Config.lua add:
+    Backgrounds = {
+        {Name = "Default Dark",  Id = "0"},
+        {Name = "Dark Anime Girl", Id = "rbxassetid://10341849885"},
+        {Name = "Smiling Dark Anime Girl", Id = "rbxassetid://6311243701"},
+        {Name = "Aesthetic Dark Anime Girl", Id = "rbxassetid://10039645009"},
+        {Name = "Scary Smily Face", Id = "rbxassetid://7255938910"},
+        {Name = "Funny Troll Face", Id = "rbxassetid://73840582821028"},
+        {Name = "Chainsaw Man Girl", Id = "rbxassetid://11778372953"},
+        -- Add more: {Name = "Name", Id = "rbxassetid://ID"},
+    },
+    ActiveBackground = 1,  -- index into Backgrounds table
+
+    MinimizeIcons = {
+        {Name = "Default",    Id = "0"},
+        {Name = "Black Hair Anime Girl",    Id = "rbxassetid://7043301688"},
+        {Name = "Anime Girl Heart Eye",    Id = "rbxassetid://8942361531"},
+        {Name = "Aesthetic Pink Anime Girl",    Id = "rbxassetid://10983517742"},
+        {Name = "Scary White Face",    Id = "rbxassetid://1452754142"},
+        {Name = "Funny Weird Face",    Id = "rbxassetid://668752955"},
+    },
+    ActiveMinimizeIcon = 1,
 
     Icon_Main          = "rbxassetid://13060262582",
     Icon_Aimbot        = "rbxassetid://11738671951",
