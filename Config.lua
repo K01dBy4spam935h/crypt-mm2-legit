38e8f08db1341c2128d87ea46f6dfe4b3a272c40
K01dBy4spam935h <junjunlahpai@gmail.com>
2026-07-17 21:41:02 +1200

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,46 @@
+-- Crypt-MM2-Legit | Config
+
+_G.Config = {
+    Backgrounds = {
+        {Name = "Default Dark", Id = "0"},
+        {Name = "Default Dark", Id = "rbxassetid://132155327"},
+        {Name = "Dark Mist",     Id = "rbxassetid://8166976502"},
+        {Name = "Dark Gradient",     Id = "rbxassetid://52784231"},
+        {Name = "Aesthetic Dark Anime Girl",     Id = "rbxassetid://10341849885"},
+        {Name = "Chainsaw Man Girl",     Id = "rbxassetid://11778372953"},
+        {Name = "Dark Smiling Anime Girl",     Id = "rbxassetid://6311243701"},
+        {Name = "Scary Smily Face",     Id = "rbxassetid://7255938910"},
+        {Name = "Troll Face",     Id = "rbxassetid://6252984672"},
+    },
+    ActiveBackground = 1,
+
+    MinimizeIcons = {
+        {Name = "Default Crypt Logo", Id = "rbxassetid://129124430266882"},
+        {Name = "Defualt Dark",  Id = "rbxassetid://0"},
+        {Name = "Defualt White",  Id = "rbxassetid://132155327"},
+        {Name = "Dark Gradient",  Id = "rbxassetid://6096768876"},
+        {Name = "Pink Smiling Anime Girl",  Id = "rbxassetid://10149736922"},
+        {Name = "Pink Heart Eye Anime Girl",  Id = "rbxassetid://8942361531"},
+        {Name = "Black Hair Anime Girl",  Id = "rbxassetid://7043301688"},
+        {Name = "Kanye West",  Id = "rbxassetid://5649884823"},
+        {Name = "Scary Smily Face",  Id = "rbxassetid://6182991657"},
+        {Name = "MM2 Logo",  Id = "rbxassetid://838484753"},
+    },
+    ActiveMinimizeIcon = 1,
+
+    TabIcons = {
+        Main        = "rbxassetid://11347112419",
+        Sheriff     = "rbxassetid://16029076062",
+        Murderer    = "rbxassetid://6187718258",
+        Visuals     = "rbxassetid://16149070679",
+        LocalPlayer = "rbxassetid://124871982298256",
+        Farm        = "rbxassetid://9008236350",
+        Teleport    = "rbxassetid://230820640",
+        Settings    = "rbxassetid://11932591121",
+    },
+
+    ToggleKey    = Enum.KeyCode.RightControl,
+    WindowSize   = UDim2.new(0, 560, 0, 440),
+    IconSize     = UDim2.new(0, 54,  0, 54),
+    StartPos     = UDim2.new(0.5, -280, 0.5, -220),
+}
